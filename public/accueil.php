4bcb77a498e08c547747e145974399dc599cfd46
Flavio <flavio.tavernier2@gmail.com>
2025-01-15 16:31:36 +0100

    debut remplissage entreprises

--- public/accueil.php
+++ public/accueil.php
@@ -11,7 +11,7 @@ include "header.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/accueil.css">
-    <link rel="stylesheet" href="../style/cardProjets.css">
+    <!-- <link rel="stylesheet" href="../style/cardProjets.css"> -->
     <title>Document</title>
 </head>
 <body>
