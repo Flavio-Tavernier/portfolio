c8ab3352e130dbea964e412e847534235fd87b95
Flavio <flavio.tavernier2@gmail.com>
2025-02-09 08:13:04 +0100

    responsive tablet accueil OK

--- public/accueil.php
+++ public/accueil.php
@@ -11,7 +11,6 @@ include "header.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/accueil.css">
-    <!-- <link rel="stylesheet" href="../style/cardProjets.css"> -->
     <title>Document</title>
 </head>
 <body>
