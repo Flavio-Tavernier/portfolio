be5c30c934d8c7211d0c7d2a0ee90be0adae0015
Flavio <flavio.tavernier2@gmail.com>
2025-02-09 11:22:02 +0100

    responsive smartphone OK

--- public/js/commun.js
+++ public/js/commun.js
@@ -12,3 +12,10 @@ function manageCardContentToDisplay(contentForCard) {
     $(".wrapper").html("");
     $(".wrapper").append(contentForCard);
 }
+
+
+function navbarSmartphoneOnClick() {
+    $("#menu-bar").toggleClass("change");
+    $("#nav").toggleClass("change");
+    $("#menu-bg").toggleClass("change-bg");
+}
